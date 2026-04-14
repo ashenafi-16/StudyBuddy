601d8a2fb7ccd77cfd1ed1b80eb43164acf76ec1
ashenafi-16 <ashumulugeta016@gmail.com>
2026-04-15 01:10:07 +0300

    update production setting

--- backend/studybuddy/settings.py
+++ backend/studybuddy/settings.py
@@ -310,3 +310,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
