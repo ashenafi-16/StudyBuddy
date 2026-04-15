76851cc65fbac9f5c02bd89721c4dfddcb116326
ashenafi-16 <ashumulugeta016@gmail.com>
2026-04-15 16:00:52 +0300

    update setting

--- backend/studybuddy/settings.py
+++ backend/studybuddy/settings.py
@@ -161,8 +161,9 @@ SESSION_CACHE_ALIAS = "default"
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
